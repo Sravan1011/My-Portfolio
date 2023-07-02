f4c89487f685fcad3110aacb773cb6ac5b68aacc
Sravan1011 <81159574+Sravan1011@users.noreply.github.com>
2023-07-02 17:44:35 +0530

    Portfolio

---
+++ my-portfolio/src/app/page.js
@@ -0,0 +1,132 @@
+import Image from 'next/image'
+import Head from 'next/head'
+import Link from 'next/link'
+import image from 'next/image'
+
+export default function Home() {
+  return ( 
+    <>
+    <title>Sravan</title>
+    <div class='h-screen flex justify-left'>
+      <div class='mt-28 divide-y divide-gray-200 max-w-3xl mx-16 '>
+        <div class='space-y-18 pt-25 pb-20 md:space-y-5'>
+          <section >
+            <div class='container mx-auto flex flex-col items-left'>
+              <div class='flex w-full flex-col items-start text-left'>
+                
+                <h1 class='class="text-3xl text-white font-bold'>Hi👋</h1>
+                <p class='my-2 text-left font-serif text-base leading-relaxed text-gray-200 sm:text-lg'>
+                  I'm Sravan a student pursuing Electronics and Telecommunication engineering who
+                  into generative ai and saas web apps . Working on coding and learning on
+                  new skills and making a good products.
+                </p>
+                <p class='my-2 text-left font-serif text-base leading-relaxed text-gray-200 sm:text-lg'>
+                  I'm nineteen,exploring things and creating stuff also intrested in 
+                  playing games , building cool stuff and intrested in theories of astrophysics and 
+                  quantum computing and mechanics .
+                </p> 
+                <div class='space-y-1 pt-4 pb-6 md:space-y-3' />
+                <section>
+                  <div class='container mx-auto mb-6 flex flex-col'>
+                    <h1 class='class="text-3xl text-white font-bold' >
+                      Projects and experiments 
+                    </h1>
+                    <div class='flex w-full flex-col items-center border-b py-1 text-white md:flex-row'>
+                      <div class='w-full'>
+                      <p class='font-semibold https://twitter.com/text-gray-200'>
+                       1. Chatbot using OpenAI API 
+                      </p>
+                     </div>
+                     </div>
+                     <div class='space-y-2 pt-6 pb-8 md:space-y-5'/>
+
+                     
+                      <h1 class='class="text-3xl text-white font-bold'>
+                      Contact Me 📲
+                      </h1>
+                      <p class='mb-2 text-left font-serif text-base leading-relaxed text-gray-200 sm:text-lg'>
+                      You can get in touch with through twitter
+                      <a target='_blank'href='https://twitter.com/Sravan1011'>
+                        <span class='underline underline-offset-'>
+                          @Sravan1011
+                        </span>
+                      </a> 
+                      "You can also contact me through my mail id sravanavvaru@gmail.com
+                      and my contact no 8956999808"
+
+                      </p>
+                      
+
+                    
+
+                     
+
+
+                      
+                        
+
+                      
+                     
+
+                    
+                    
+
+                  </div>
+                </section>
+
+
+
+              </div>
+
+
+            </div>
+
+          </section>
+
+        </div>
+
+      </div>
+
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    )
+
+
+
+
+
+
+
+}
+  
+
+
+
+
+
+
+
+
+         
+        
+      
+    
