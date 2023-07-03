69b851bba1512aefca32144e49fa300a5a6ff71e
Prajwal <73160418+prajwxlk@users.noreply.github.com>
2023-07-03 13:23:14 +0000

    class to className

--- src/app/page.js
+++ src/app/page.js
@@ -7,47 +7,47 @@ export default function Home() {
   return ( 
     <>
     <title>Sravan</title>
-    <div class='h-screen flex justify-left'>
-      <div class='mt-28 divide-y divide-gray-200 max-w-3xl mx-16 '>
-        <div class='space-y-18 pt-25 pb-20 md:space-y-5'>
+    <div className='h-screen flex justify-left'>
+      <div className='mt-28 divide-y divide-gray-200 max-w-3xl mx-16 '>
+        <div className='space-y-18 pt-25 pb-20 md:space-y-5'>
           <section >
-            <div class='container mx-auto flex flex-col items-left'>
-              <div class='flex w-full flex-col items-start text-left'>
+            <div className='container mx-auto flex flex-col items-left'>
+              <div className='flex w-full flex-col items-start text-left'>
                 
-                <h1 class='class="text-3xl text-white font-bold'>Hi👋</h1>
-                <p class='my-2 text-left font-serif text-base leading-relaxed text-gray-200 sm:text-lg'>
+                <h1 className='className="text-3xl text-white font-bold'>Hi👋</h1>
+                <p className='my-2 text-left font-serif text-base leading-relaxed text-gray-200 sm:text-lg'>
                   I'm Sravan a student pursuing Electronics and Telecommunication engineering who
                   into generative ai and saas web apps . Working on coding and learning on
                   new skills and making a good products.
                 </p>
-                <p class='my-2 text-left font-serif text-base leading-relaxed text-gray-200 sm:text-lg'>
+                <p className='my-2 text-left font-serif text-base leading-relaxed text-gray-200 sm:text-lg'>
                   I'm nineteen,exploring things and creating stuff also intrested in 
                   playing games , building cool stuff and intrested in theories of astrophysics and 
                   quantum computing and mechanics .
                 </p> 
-                <div class='space-y-1 pt-4 pb-6 md:space-y-3' />
+                <div className='space-y-1 pt-4 pb-6 md:space-y-3' />
                 <section>
-                  <div class='container mx-auto mb-6 flex flex-col'>
-                    <h1 class='class="text-3xl text-white font-bold' >
+                  <div className='container mx-auto mb-6 flex flex-col'>
+                    <h1 className='className="text-3xl text-white font-bold' >
                       Projects and experiments 
                     </h1>
-                    <div class='flex w-full flex-col items-center border-b py-1 text-white md:flex-row'>
-                      <div class='w-full'>
-                      <p class='font-semibold https://twitter.com/text-gray-200'>
+                    <div className='flex w-full flex-col items-center border-b py-1 text-white md:flex-row'>
+                      <div className='w-full'>
+                      <p className='font-semibold https://twitter.com/text-gray-200'>
                        1. Chatbot using OpenAI API 
                       </p>
                      </div>
                      </div>
-                     <div class='space-y-2 pt-6 pb-8 md:space-y-5'/>
+                     <div className='space-y-2 pt-6 pb-8 md:space-y-5'/>
 
                      
-                      <h1 class='class="text-3xl text-white font-bold'>
+                      <h1 className='className="text-3xl text-white font-bold'>
                       Contact Me 📲
                       </h1>
-                      <p class='mb-2 text-left font-serif text-base leading-relaxed text-gray-200 sm:text-lg'>
+                      <p className='mb-2 text-left font-serif text-base leading-relaxed text-gray-200 sm:text-lg'>
                       You can get in touch with through twitter
                       <a target='_blank'href='https://twitter.com/Sravan1011'>
-                        <span class='underline underline-offset-'>
+                        <span className='underline underline-offset-'>
                           @Sravan1011
                         </span>
                       </a> 
